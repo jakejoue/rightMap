eed5c22a6113027c991a1e4b66c620664ae07f74
jakejoue <914316962@qq.com>
2018-03-12 15:57:08 +0800

    webservice和代理配置

--- src/assets/js/server/webService.js
+++ src/assets/js/server/webService.js
@@ -3,15 +3,14 @@ import axios from 'axios';
 
 export default class WebService {
   constructor(proxyUrl, webServiceUrl) {
-    // this.proxyUrl = proxyUrl;
-    // this.webServiceUrl = webServiceUrl;
     this.axios = axios.create({
-      baseURL: proxyUrl + '?' + webServiceUrl,
-      responseType: 'text'
+      // baseURL: proxyUrl + '?' + webServiceUrl,
+      baseURL: '/proxy' + webServiceUrl,
+      responseType: 'document'
     });
   };
 
-  // 采集员查询
+  // 采集员
   findAllOnLineObserverJson() {
     var data = '<?xml version="1.0" encoding="utf-8"?>' +
       '<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">' +
@@ -29,11 +28,18 @@ export default class WebService {
     return this.invoke("/ObsBusinessService/findAllOnLineObserverJson", options);
   };
 
+  // 查询
   invoke(url, { namespace, method, data, dataType }) {
     return this.axios.post(url, data, {
       headers: { SOAPAction: namespace + method }
-    }).then(respones => {
-      console.log(respones);
+    }).then(({ data }) => {
+      try {
+        data = data.getElementsByTagName('return')[0];
+        const result = JSON.parse(data.textContent);
+        console.log(result);
+      } catch (error) {
+        return [];
+      }
     })
   }
 }
