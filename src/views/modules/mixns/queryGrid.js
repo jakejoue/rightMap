fe4500bb38978666ac20c60cfeaef39efcf37470
jakejoue <914316962@qq.com>
2018-03-29 18:24:24 +0800

    代码格式修改

--- src/views/modules/mixns/queryGrid.js
+++ src/views/modules/mixns/queryGrid.js
@@ -1,48 +1,46 @@
+async function query_(type, value) {
+  switch (type) {
+    case "兴趣点":
+      return query.queryPoint(value, 0, 1000).then(data => {
+        data.items.forEach(item => {
+          item.geo = KMap.Geometry.toWKT(
+            new KMap.Point([item.x, item.y])
+          );
+          item.name = item.name || "";
+          item.address = item.address || "";
+          item.tel = item.tel || "";
+        });
+        return data;
+      });
+      break;
+    case "道路":
+      return query.queryRoad(value, 0, 100);
+      break;
+    case "行政区":
+      return query.queryGrid(value, "1", 0, 100);
+      break;
+    case "街道办":
+      return query.queryGrid(value, "2", 0, 100);
+      break;
+    case "社区":
+      return query.queryGrid(value, "3", 0, 100);
+      break;
+    case "工作网格":
+      return query.queryGrid(value, "4", 0, 100);
+      break;
+    case "单元网格":
+      return query.queryGrid(value, "5", 0, 100);
+      break;
+    default:
+      throw new Error("未定义的查找的类型：" + searchType);
+  }
+}
+
 // 网格查询共用方法
 export default {
   methods: {
     queryGrid(type, value) {
-      switch (type) {
-        case "兴趣点":
-          query.queryPoint(value, 0, 1000).then(data => {
-            data.items.forEach(item => {
-              item.geo = KMap.Geometry.toWKT(
-                new KMap.Point([item.x, item.y])
-              );
-              item.name = item.name || "";
-              item.address = item.address || "";
-              item.tel = item.tel || "";
-            });
-            return data;
-          }).then(this.showQueryTaskResults).catch(this.queryTaskErrorResults);
-          break;
-        case "道路":
-          query.queryRoad(value, 0, 100).then(this.showQueryTaskResults).catch(this.queryTaskErrorResults);
-          break;
-        case "行政区":
-          query
-            .queryGrid(value, "1", 0, 100).then(this.showQueryTaskResults).catch(this.queryTaskErrorResults);
-          break;
-        case "街道办":
-          query
-            .queryGrid(value, "2", 0, 100).then(this.showQueryTaskResults).catch(this.queryTaskErrorResults);
-          break;
-        case "社区":
-          query
-            .queryGrid(value, "3", 0, 100).then(this.showQueryTaskResults).catch(this.queryTaskErrorResults);
-          break;
-        case "工作网格":
-          query
-            .queryGrid(value, "4", 0, 100).then(this.showQueryTaskResults).catch(this.queryTaskErrorResults);
-          break;
-        case "单元网格":
-          query
-            .queryGrid(value, "5", 0, 100).then(this.showQueryTaskResults).catch(this.queryTaskErrorResults);
-          break;
-        default:
-          console.log("未定义的查找的类型：" + searchType);
-          return false;
-      }
+      query_(type, value).then(this.showQueryTaskResults).catch(this.queryTaskErrorResults);
     },
     queryTaskErrorResults(err) {
       this.loading(false);
